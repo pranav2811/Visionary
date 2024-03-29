0059adc7025feddd469a936f2473450f35b8632b
Pranav <powar.pranav29@gmail.com>
2024-03-29 14:56:43 +0530

    landing page added

--- lib/Screens/landingpage.dart
+++ lib/Screens/landingpage.dart
@@ -1,2 +1,189 @@
 import 'package:flutter/material.dart';
 
+class LandingPage extends StatefulWidget {
+  const LandingPage({super.key});
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.only(top: 100.0, left: 10.0),
+        height: height,
+        width: width,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: width,
+                height: height * 0.45,
+                child: Image.asset(
+                  'assets/images/landing.jpeg',
+                  fit: BoxFit.fill,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // Handle "I Need Assistance" button press
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Color(0xffEE7B23),
+                  padding:
+                      EdgeInsets.symmetric(vertical: 15.0, horizontal: 30.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                ),
+                child: Text(
+                  'I Need Assistance',
+                  style: TextStyle(fontSize: 16.0),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () {
+                  // Handle "I Am Here to Volunteer" button press
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Color(0xffEE7B23),
+                  padding:
+                      EdgeInsets.symmetric(vertical: 15.0, horizontal: 30.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                ),
+                child: Text(
+                  'I Am Here to Volunteer',
+                  style: TextStyle(fontSize: 16.0),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Second()));
+                },
+                child: Text.rich(
+                  TextSpan(text: 'Already have an account? ', children: [
+                    TextSpan(
+                      text: 'SignIn',
+                      style: TextStyle(color: Color(0xffEE7B23)),
+                    ),
+                  ]),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Second extends StatefulWidget {
+  @override
+  _SecondState createState() => _SecondState();
+}
+
+class _SecondState extends State<Second> {
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Container(
+        height: height,
+        width: width,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: width,
+                height: height * 0.45,
+                child: Image.asset(
+                  'assets/images/landing.jpeg',
+                  fit: BoxFit.fill,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Signup',
+                      style: TextStyle(
+                          fontSize: 25.0, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  suffixIcon: Icon(Icons.email),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  suffixIcon: Icon(Icons.visibility_off),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Forget password?',
+                      style: TextStyle(fontSize: 12.0),
+                    ),
+                    ElevatedButton(
+                      child: Text('Signup'),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xffEE7B23),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20.0),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
