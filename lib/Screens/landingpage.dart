460271b3e52766709c1c1f6f55e59a3e4b9677be
Pranav <powar.pranav29@gmail.com>
2024-05-24 23:22:04 +0530

    anonymous signin implemented

--- lib/Screens/landingpage.dart
+++ lib/Screens/landingpage.dart
@@ -1,4 +1,9 @@
+import 'package:blindapp/Screens/userHomePage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
 import 'package:blindapp/Screens/loginPage.dart';
 import 'package:blindapp/Screens/userSignupPage.dart';
 import 'package:blindapp/Screens/volunteerSignupPage.dart';
@@ -11,10 +16,124 @@ class LandingPage extends StatefulWidget {
 }
 
 class _LandingPageState extends State<LandingPage> {
+  final FlutterTts flutterTts = FlutterTts();
+  final stt.SpeechToText _speech = stt.SpeechToText();
+  bool _isListening = false;
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeTTSAndSTT();
+  }
+
+  Future<void> _initializeTTSAndSTT() async {
+    await _speak(
+        "Welcome to BlindApp. Say 'Assistance' or 'Volunteer' to navigate.");
+    _startListening();
+  }
+
+  Future<void> _speak(String text) async {
+    await flutterTts.speak(text);
+  }
+
+  void _startListening() async {
+    bool available = await _speech.initialize(
+      onStatus: (status) {
+        print('onStatus: $status');
+        if (status == 'done' || status == 'notListening') {
+          _tryRestartListening();
+        }
+      },
+      onError: (errorNotification) {
+        print('onError: $errorNotification');
+        if (errorNotification.errorMsg == "error_speech_timeout") {
+          _tryRestartListening();
+        }
+      },
+    );
+    if (available) {
+      _speech.listen(
+          onResult: (result) {
+            setState(() {
+              _isListening = false;
+              _handleCommand(result.recognizedWords.toLowerCase());
+            });
+          },
+          listenFor: Duration(seconds: 30),
+          pauseFor: Duration(seconds: 5),
+          partialResults: true);
+    }
+  }
+
+  void _tryRestartListening() {
+    if (!_isListening) {
+      // Check to ensure we're not already listening
+      _isListening = true;
+      _startListening();
+    }
+  }
+
+  void _handleCommand(String command) {
+    if (command.contains("assistance")) {
+      _speak("You have opted for the assistance option");
+      _registerGuest();
+    } else if (command.contains("volunteer")) {
+      _speak("I am here to volunteer selected.");
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => VolunterSignup()));
+    } else {
+      _speak("Command not recognized. Please say 'Assistance' or 'Volunteer'.");
+    }
+  }
+
+  Future<void> _registerGuest() async {
+    try {
+      UserCredential userCredential = await _auth.signInAnonymously();
+      User? user = userCredential.user;
+
+      if (user != null) {
+        DocumentReference counterRef =
+            _firestore.collection('metadata').doc('guestCounter');
+        DocumentSnapshot counterSnapshot = await counterRef.get();
+        int guestCount = 0;
+        if (counterSnapshot.exists) {
+          guestCount = counterSnapshot['count'] as int;
+        }
+        guestCount += 1;
+        await counterRef.set({'count': guestCount});
+        String guestName = 'Guest$guestCount';
+        await _firestore.collection('users').doc(user.uid).set({
+          'name': guestName,
+          'email': 'guest@blindapp.com',
+          'phone': 'N/A',
+          'role': 'guestuser',
+        });
+        _speak("Signed in as Guest User");
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => const UserHome()));
+      }
+    } catch (e) {
+      print("Failed to register guest user: $e");
+      _speak("Failed to sign in as Guest User. Please try again.");
+    }
+  }
+
+  @override
+  void dispose() {
+    _speech.stop();
+    _speech.cancel();
+    flutterTts.stop();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
+
     return Scaffold(
       body: Container(
         padding: const EdgeInsets.only(top: 100.0, left: 10.0),
@@ -24,12 +143,15 @@ class _LandingPageState extends State<LandingPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                width: width,
-                height: height * 0.45,
-                child: Image.asset(
-                  'assets/images/landing.jpeg',
-                  fit: BoxFit.fill,
+              GestureDetector(
+                onTap: _startListening,
+                child: Container(
+                  width: width,
+                  height: height * 0.45,
+                  child: Image.asset(
+                    'assets/images/landing.jpeg',
+                    fit: BoxFit.fill,
+                  ),
                 ),
               ),
               const Padding(
@@ -40,15 +162,14 @@ class _LandingPageState extends State<LandingPage> {
               ),
               ElevatedButton(
                 onPressed: () {
-                  // Handle "I Need Assistance" button press
-                  //route to user_assistance.dart
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => const UserSignup()));
+                  _speak(
+                      "I need assistance button pressed. Loading assistance page.");
+                  _registerGuest();
                 },
                 style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.blue, //Color(0xffEE7B23),
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 15.0, horizontal: 30.0),
+                  foregroundColor: Colors.blue,
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 15.0, horizontal: 30.0),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(20.0),
                   ),
@@ -61,14 +182,15 @@ class _LandingPageState extends State<LandingPage> {
               const SizedBox(height: 20.0),
               ElevatedButton(
                 onPressed: () {
-                  // Handle "I Am Here to Volunteer" button press
+                  _speak(
+                      "I am here to volunteer button pressed. Loading volunteer page.");
                   Navigator.push(
                       context,
                       MaterialPageRoute(
                           builder: (context) => VolunterSignup()));
                 },
                 style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.blue, //Color(0xffEE7B23),
+                  foregroundColor: Colors.blue,
                   padding: const EdgeInsets.symmetric(
                       vertical: 15.0, horizontal: 30.0),
                   shape: RoundedRectangleBorder(
@@ -83,15 +205,15 @@ class _LandingPageState extends State<LandingPage> {
               const SizedBox(height: 20.0),
               GestureDetector(
                 onTap: () {
+                  _speak("Navigating to sign in page.");
                   Navigator.push(context,
                       MaterialPageRoute(builder: (context) => LoginPage()));
                 },
                 child: const Text.rich(
                   TextSpan(text: 'Already have an account? ', children: [
                     TextSpan(
-                      text: 'SignIn',
-                      style:
-                          TextStyle(color: Colors.blue), //Color(0xffEE7B23)),
+                      text: 'Sign In',
+                      style: TextStyle(color: Colors.blue),
                     ),
                   ]),
                 ),
