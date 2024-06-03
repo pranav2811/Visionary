0c173bafb552fc6ad1f2dff30273546c00bfc58f
Pranav <powar.pranav29@gmail.com>
2024-06-04 02:37:53 +0530

    default usercamera direction set to rear

--- lib/Screens/userVideoCallPage.dart
+++ lib/Screens/userVideoCallPage.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:agora_uikit/agora_uikit.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 
 class VideoCallScreen extends StatefulWidget {
   const VideoCallScreen({super.key});
@@ -51,6 +52,9 @@ class _VideoCallScreenState extends State<VideoCallScreen> {
 
     await _client!.initialize();
 
+    _client!.engine.setCameraCapturerConfiguration(CameraCapturerConfiguration(
+        cameraDirection: CameraDirection.cameraRear));
+
     setState(() {}); // Refresh the UI
   }
 
@@ -90,79 +94,3 @@ class _VideoCallScreenState extends State<VideoCallScreen> {
     );
   }
 }
-
-
-
-
-
-// import 'package:flutter/material.dart';
-// import 'package:agora_rtc_engine/agora_rtc_engine.dart';
-// import 'package:agora_uikit/agora_uikit.dart';
-
-// class VideoCallScreen extends StatefulWidget {
-//   const VideoCallScreen({super.key});
-
-//   @override
-//   State<VideoCallScreen> createState() => _VideoCallScreenState();
-// }
-
-// class _VideoCallScreenState extends State<VideoCallScreen> {
-//   final AgoraClient _client = AgoraClient(
-//     agoraConnectionData: AgoraConnectionData(
-//         appId: '5a1eb9ed6ab34eada8cbd564a0f5732c',
-//         channelName: 'testing',
-//         tempToken:
-//             '007eJxTYGg5npXpKNz/8oSK9HWf9de2/j9zOvrv0bkXtgfaTk80/7RTgcE00TA1yTI1xSwxydgkNTEl0SI5KcXUzCTRIM3U3NgoOUk8Lq0hkJEhe/1tBkYoBPHZGUpSi0sy89IZGADFqyRT'),
-//   );
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     _initAgora();
-//   }
-
-//   Future<void> _initAgora() async {
-//     await _client.initialize();
-//     await _client.engine.setCameraCapturerConfiguration(
-//       const CameraCapturerConfiguration(
-//         cameraDirection: CameraDirection.cameraRear,
-//       ),
-//     );
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return WillPopScope(
-//       onWillPop: () async => false,
-//       child: Scaffold(
-//         backgroundColor: Colors.black,
-//         body: SafeArea(
-//           child: Stack(
-//             children: [
-//               AgoraVideoViewer(
-//                 client: _client,
-//                 layoutType: Layout
-//                     .oneToOne, // This may need to be adjusted if available layouts do not meet your requirements
-//                 // enableToggleLocalVideoSize: true,
-//                 showNumberOfUsers: false,
-//               ),
-//               Positioned(
-//                 bottom: 20,
-//                 right: 20,
-//                 left: 20,
-//                 child: AgoraVideoButtons(
-//                   client: _client,
-//                   enabledButtons: const [
-//                     BuiltInButtons.switchCamera,
-//                     BuiltInButtons.callEnd,
-//                     BuiltInButtons.toggleMic,
-//                   ],
-//                 ),
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
